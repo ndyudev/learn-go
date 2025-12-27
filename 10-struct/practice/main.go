22bdfea9271bb0b7436de420e9820b9a9eb7d3eb
Chauu Nhat Duyy ( Dyu ) <119422970+ChauuNhatDuyyIT@users.noreply.github.com>
2025-12-27 22:38:13 +0700

    Learn Golang: Practice Go

--- 10-struct/practice/main.go
+++ 10-struct/practice/main.go
@@ -7,20 +7,38 @@ type HinhChuNhat struct {
 	chieuRong float32 `desc:"Chieu rong hinh chu nhat"`
 }
 
-func (hcn HinhChuNhat) getChuVi() float32 {
+func (hcn *HinhChuNhat) getChuVi() float32 {
 	return (hcn.chieuRong + hcn.chieuDai) * 2
 
 }
 
-func (hcn HinhChuNhat) getDienTich() float32 {
+func (hcn *HinhChuNhat) getDienTich() float32 {
 	return hcn.chieuRong * hcn.chieuDai
 }
 
 func main() {
-	hcn := HinhChuNhat{
-		chieuDai:  5,
-		chieuRong: 7,
+	var hinhChuNhat HinhChuNhat
+
+	for {
+		fmt.Println("Nhập chiều dài hình chữ nhật:")
+		_, err := fmt.Scanf("%f", &hinhChuNhat.chieuDai)
+
+		if err == nil && hinhChuNhat.chieuDai > 0 {
+			break
+		}
+		fmt.Println("Kích thước chiều rộng phải lớn hơn 0 !")
+
 	}
-	fmt.Println(hcn.getChuVi())
-	fmt.Println(hcn.getDienTich())
+
+	for {
+		fmt.Println("Nhập chiều rộng hình chữ nhật:")
+		_, err := fmt.Scanf("%f", &hinhChuNhat.chieuRong)
+
+		if err == nil && hinhChuNhat.chieuRong > 0 {
+			break
+		}
+		fmt.Println("Kích thước chiều dài phải lớn hơn 0 !")
+	}
+	fmt.Println(hinhChuNhat.getChuVi())
+	fmt.Println(hinhChuNhat.getDienTich())
 }
